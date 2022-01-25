375fa6ae289a5548ca556208e1699d44476bfcfe
luifer Almendrales <almendralesluifer@gmail.com>
2022-01-25 10:33:40 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -39,4 +39,27 @@ export default {
 a.router-link-exact-active {
   color: #42b983;
 }
+@media all and (device-width: 768px) and (device-height: 1024px) and (orientation: portrait) {
+  .foo {
+    height: 1024px;
+  }
+}
+
+/* 
+     iPad landscape orientation.
+    */
+@media all and (device-width: 768px) and (device-height: 1024px) and (orientation: landscape) {
+  .foo {
+    height: 768px;
+  }
+}
+
+/* También se pueden fijar por su "aspect ratio".
+     iPhone 5
+    */
+@media screen and (device-aspect-ratio: 40/71) {
+  .foo {
+    height: 500px;
+  }
+}
 </style>
