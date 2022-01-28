633544685a1c34ca612bdd56c0ab92ca2608cb12
luifer Almendrales <almendralesluifer@gmail.com>
2022-01-28 13:45:07 -0500

    config term and coditions

--- src/views/TermsAndConditions.vue
+++ src/views/TermsAndConditions.vue
@@ -2,56 +2,31 @@
   <div class="container wow fadeInUp">
     <h3 class="text-primary">Términos y condiciones</h3>
     <br />
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi odio
-      similique neque animi aliquam officiis sequi tempore, optio labore
-      exercitationem error et inventore explicabo illo necessitatibus molestiae
-      quia quisquam facere earum magni omnis totam! Voluptatem iusto nobis
-      distinctio quos ipsam quo sed maiores esse voluptate odit explicabo fuga
-      assumenda provident fugiat cumque, deserunt alias necessitatibus eligendi
-      veniam quidem! Molestias omnis quis repellendus ullam magni suscipit
-      reiciendis voluptates asperiores cupiditate id dolorum tenetur ipsum quas,
-      quibusdam, nemo error? Sunt corrupti temporibus obcaecati reprehenderit
-      recusandae iure eligendi mollitia. Accusantium cumque, voluptatum
-      quibusdam animi dolor maiores veritatis, deserunt consequatur nisi quae
-      officiis excepturi nulla fuga repellendus. Beatae voluptatum fuga, magnam
-      cupiditate aspernatur mollitia numquam eum excepturi, sint adipisci quidem
-      quia accusantium explicabo cum ratione culpa natus reprehenderit est
-      doloremque vel quod optio maiores. Sit harum neque iusto, laboriosam optio
-      consectetur eveniet, repudiandae, molestiae placeat provident labore?
-      Ducimus, optio asperiores ipsa vero temporibus delectus dolor magnam,
-      atque modi placeat voluptatum quidem cum! Praesentium iure laborum fugit
-      totam aliquam aperiam veritatis sit sed sunt optio dolorem ad quo ipsa
-      recusandae ipsam, tenetur dolor atque ducimus deserunt voluptates et sint.
-      Ducimus molestiae, reprehenderit hic fugit illum quasi sit quam molestias?
-      Quos reiciendis itaque labore optio magnam praesentium qui nihil sit
-      doloribus laboriosam quaerat, inventore dolor unde? Magnam dolor odio
-      rerum soluta, quod illum sunt doloribus. Error ipsum pariatur magni ad
-      rerum unde dolores! Excepturi ducimus voluptatem voluptas molestias ad sed
-      ipsam inventore voluptatum vitae distinctio beatae veniam, magni
-      explicabo, laudantium odit soluta ratione tenetur! Totam dolor sequi
-      pariatur in numquam vitae placeat vel exercitationem cupiditate corporis
-      unde itaque optio tenetur, eaque dignissimos. Maiores accusamus repellat
-      iusto rem? Ipsa, assumenda obcaecati! Necessitatibus odit voluptas
-      cupiditate incidunt illo quaerat vel adipisci dolorem perspiciatis. Nam
-      reprehenderit eos iste tenetur culpa facere accusantium voluptatum aperiam
-      perferendis blanditiis sit facilis at, earum, ducimus commodi velit natus
-      nobis, soluta molestias? Sit maiores corporis dicta, dolor provident
-      necessitatibus ipsum accusantium qui molestiae nam aspernatur culpa optio
-      recusandae, non quidem aut eveniet ea id, illo quibusdam obcaecati!
-      Eligendi atque, adipisci officiis placeat quas ad rerum perferendis quis
-      unde corporis aspernatur, harum dicta dignissimos, eveniet obcaecati
-      accusamus vitae earum dolor similique voluptatum. Unde, explicabo dicta?
-      Eveniet quo perspiciatis delectus impedit laudantium sequi omnis at
-      dolorum maiores distinctio, officiis quia minima nostrum commodi, animi
-      sint ipsa sapiente libero. Dignissimos, non voluptatibus. Optio libero
-      architecto unde rem, beatae omnis eos itaque repellendus sit vel harum
-      nesciunt perferendis?
-    </p>
+    <div v-html="terAndConditions"></div>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
-  name:"TermsAndConditions",
-}
+  name: "TermsAndConditions",
+  data() {
+    return {
+      urlApi: process.env.VUE_APP_URL_API_ADMIN,
+      terAndConditions: null,
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      try {
+        const res = await axios.get(this.urlApi + "term-and-condition-view");
+        this.terAndConditions = res.data.data.content;
+      } catch (error) {
+        console.log(error.response);
+      }
+    },
+  },
+};
 </script>
